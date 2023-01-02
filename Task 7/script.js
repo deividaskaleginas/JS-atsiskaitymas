597538eb310142279099067ab40ede2917469a0a
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-01-02 11:26:46 +0200

    Task7

--- Task 7/script.js
+++ Task 7/script.js
@@ -5,8 +5,15 @@ Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objektą
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
 const audi = {
-  marke: 'audi',
-  model: 'A6',
+  marke: "audi",
+  model: "A6",
   year: 2005,
-  color: 'white'
+  color: "white",
 };
+
+showObjectKeys = (object) => {
+  const keys = Object.keys(object);
+  console.log(keys);
+};
+
+showObjectKeys(audi);
