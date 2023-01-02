92cbf14d1107f8f71e00bacb1115e78c61f87306
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-01-02 11:33:08 +0200

    Task8

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,28 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+  constructor(number1, number2) {
+    this.number1 = number1;
+    this.number2 = number2;
+  }
+
+  sum() {
+    return this.number1 + this.number2;
+  }
+  subtraction() {
+    return this.number1 - this.number2;
+  }
+  multiplication() {
+    return this.number1 * this.number2;
+  }
+  division() {
+    return this.number1 / this.number2;
+  }
+}
+
+console.log(new Calculator(2, 2).sum());
+console.log(new Calculator(2, 2).subtraction());
+console.log(new Calculator(2, 2).multiplication());
+console.log(new Calculator(2, 2).division());
