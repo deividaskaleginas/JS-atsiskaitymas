4e2b0b0a51da747027b980d3f0a1a533eed51ce2
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-01-02 11:24:32 +0200

    Task6

--- Task 6/script.js
+++ Task 6/script.js
@@ -9,13 +9,29 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
-  { id: '3', name: 'Tom Jones', age: 31 },
-  { id: '4', name: 'Rose Peterson', age: 17 },
-  { id: '5', name: 'Alex John', age: 25 },
-  { id: '6', name: 'Ronald Jones', age: 63 },
-  { id: '7', name: 'Elton Smith', age: 16 },
-  { id: '8', name: 'Simon Peterson', age: 30 },
-  { id: '9', name: 'Daniel Cane', age: 51 },
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
 ];
+
+getUserAverageAge = (usersArray) => {
+  const usersAges = usersArray.map((item) => item.age);
+  const usersAverage =
+    usersAges.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    ) / usersAges.length;
+  console.log(usersAverage);
+};
+getUserAverageAge(users);
+
+getUsersNames = (usersArray) => {
+  const usersNameList = usersArray.map((item) => item.name);
+  console.log(usersNameList);
+};
+getUsersNames(users);
