fc7ac7e618b16df2f95392183d5a3883b9ff620c
deividaskaleginas <deividas.kaleginas@gmail.com>
2023-01-02 10:25:39 +0200

    Task3

--- Task 3/script.js
+++ Task 3/script.js
@@ -9,5 +9,27 @@ Paspaudus mygtuką "Show users":
 
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
+const button = document.querySelector("#btn");
+const message = document.querySelector("#message");
+const output = document.querySelector("#output");
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  button.setAttribute("class", "none");
+  message.setAttribute("class", "none");
 
-const ENDPOINT = 'https://api.github.com/users';
+  fetch("https://api.github.com/users")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((element) => {
+        const div = document.createElement("div");
+        const p = document.createElement("p");
+        const img = document.createElement("img");
+        const pText = document.createTextNode(element.login);
+
+        p.append(pText);
+        img.setAttribute("src", `${element.avatar_url}`);
+        div.append(p, img);
+        output.append(div);
+      });
+    });
+});
